cd191a50931e8259d5b76b00dfd8be2b9836daed
Pritz Belleza <pritz.belleza@gmail.com>
2020-10-22 16:56:07 +0800

    add fetching of jokes

--- src/scenes/App/App.tsx
+++ src/scenes/App/App.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Button from 'components/button/Button';
 import './App.css';
 
+interface Joke {
+  id: string;
+  joke: string;
+  status: number;
+}
+
 function App() {
+  const [joke, setJoke] = useState<Joke>();
+
+  const generateDadJoke = (): void => {
+    fetch('https://icanhazdadjoke.com/', {
+        method: 'get', 
+        headers: {
+          'Accept': 'application/json'
+        },
+      })
+      .then(res => res.json())
+      .then(data => { console.log(data); setJoke(data) })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
   return (
     <div className="App">
-      App
+      {joke ? <span>{joke.joke}</span> : 'Press Generate button to receive Dad jokes!'}
+      <Button text='Generate Dad Joke' onClick={generateDadJoke} />
     </div>
   );
 }
