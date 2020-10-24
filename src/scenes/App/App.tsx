14f0f755a4f5a9dc556bc1f5d8bf5bbbdc16567e
Pritz Belleza <pritz.belleza@gmail.com>
2020-10-25 01:42:19 +0800

    add joke fetching

--- src/scenes/App/App.tsx
+++ src/scenes/App/App.tsx
@@ -1,35 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Button from 'components/button/Button';
 import './App.css';
 
-interface Joke {
-  id: string;
-  joke: string;
-  status: number;
-}
-
 function App() {
-  const [joke, setJoke] = useState<Joke>();
+  const [joke, setJoke] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const generateDadJoke = (): void => {
-    fetch('https://icanhazdadjoke.com/', {
+  useEffect(() => {
+    async function fetchAndTrain() {
+      try {
+        setLoading(true);
+        const numOfFetches = 5;
+        var consolidatedData: any[] = [];
+        for(let i = 0; i < numOfFetches; i++){
+          const data = await fetchData(i);
+          consolidatedData.push(...data.results);
+        }
+        // add train here
+      } catch (err) {
+        throw(err);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchAndTrain();
+  }, [])
+
+  const generate = (): void => {
+    setJoke('sample joke');
+  }
+
+  const fetchData = async (page: number): Promise<any> => {
+    try {
+      let res = await fetch(`https://icanhazdadjoke.com/search?limit=30&page=${page}`, {
         method: 'get', 
         headers: {
           'Accept': 'application/json'
         },
-      })
-      .then(res => res.json())
-      .then(data => { console.log(data); setJoke(data) })
-      .catch(err => {
-        console.log(err);
-      })
+      });
+      let data = res.json();
+      return data;
+    } catch (err) {
+      throw(err);
+    }
   }
 
   return (
     <div className="App">
-      {joke ? <span>{joke.joke}</span> : 'Press Generate button to receive Dad jokes!'}
-      <Button text='Generate Dad Joke' onClick={generateDadJoke} />
+      {joke ? <span>{joke}</span> : 'Press Generate button to receive Dad jokes!'}
+      <Button text='Generate' disabled={loading} onClick={generate} />
     </div>
   );
 }
