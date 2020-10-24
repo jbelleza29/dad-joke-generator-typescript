56e44e8ecff7e0149b9d098b7e200eada7e6865e
Pritz Belleza <pritz.belleza@gmail.com>
2020-10-25 01:38:59 +0800

    add button component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -3,12 +3,13 @@ import React, { ReactElement } from 'react'
 interface BtnProps {
   text: string;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
+  disabled: boolean;
 }
 
-export default function Button({ text, onClick }: BtnProps): ReactElement {
+export default function Button({ text, onClick, disabled }: BtnProps): ReactElement {
   return (
     <div className='btn'>
-      <button onClick={onClick}>{text}</button>
+      <button onClick={onClick} disabled={disabled}>{text}</button>
     </div>
   )
 }
